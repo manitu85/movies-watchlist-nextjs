c4cc189395a04f50f5d4800dbb5d36267db0b183
manitu85 <manitu.psy@gmail.com>
2020-05-21 23:39:25 +0200

    setting-up project

---
+++ components/sidebar.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sidebar = () => (
+  <>
+    <h1 className="mt-3">Shop Name</h1>
+    <div className="list-group">
+      <a href="#" className="list-group-item">Category 1</a>
+      <a href="#" className="list-group-item">Category 2</a>
+      <a href="#" className="list-group-item">Category 3</a>
+    </div>
+  </>
+)
+
+export default Sidebar
---
+++ pages/about.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const About = () => <h1>Hello About Page</h1>
+
+
+export default About
