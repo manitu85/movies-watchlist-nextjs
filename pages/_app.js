05e389143f75ab76f65a1f490a5731ffd1b9d924
manitu85 <manitu.psy@gmail.com>
2020-05-23 22:50:28 +0200

    add getInitailProps to container _app and fetched from home component

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,36 @@
-// import App from 'next/app';
+import App from 'next/app';
 import Head from 'next/head'
 
 import Navbar from '../components/navbar.component'
 import Footer from '../components/footer.component'
 
+class MovieApp extends App {
 
-
-// This default export is required in a new `pages/_app.js` file.
-const App = ({ Component, pageProps }) => {
-  // execute getInitialProps and pass data to home component via props
-  return(
-    <>
-      <Head>
-        <title>Home</title>
-        <link rel='stylesheet' href='https://bootswatch.com/4/superhero/bootstrap.min.css' />
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossOrigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossOrigin="anonymous"></script>
-      </Head>
-      <Navbar />
-      <div className="site-bg home-page-padding">
-        {/* // Component holds pages where you navigation to, similar as children props */}
-        <Component {...pageProps} />
-      </div>
-      <Footer />
-      <style jsx>
-        {`
+  // execute getInitialProps and pass data to page via props
+  static getInitialProps = async (appContext) => {
+    console.log('calling initial props from _app.js');
+    const appProps = await App.getInitialProps(appContext)
+    return { ...appProps }
+  }
+  
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <>
+        <Head>
+          <title>Home</title>
+          <link rel='stylesheet' href='https://bootswatch.com/4/superhero/bootstrap.min.css' />
+          <script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossOrigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossOrigin="anonymous"></script>
+        </Head>
+        <Navbar />
+        <div className="site-bg home-page-padding">
+          {/* // Component holds pages where you navigation to, similar as children props */}
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+        <style jsx>
+          {`
             .home-page-padding {
               padding-top: 3rem;
             }
@@ -33,16 +39,57 @@ const App = ({ Component, pageProps }) => {
               background-color: #1a2b34;
             }  
         ` }
-      </style>
-    </>
-  )
+        </style>
+      </>
+    )
+  }
 }
 
-// Home.getInitialProps = async (appContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
+
+export default MovieApp
+
+
+
+// This default export is required in a new `pages/_app.js` file.
+// const App = ({ Component, pageProps }) => {
+//   // execute getInitialProps and pass data to home component via props
+//   return(
+//     <>
+//       <Head>
+//         <title>Home</title>
+//         <link rel='stylesheet' href='https://bootswatch.com/4/superhero/bootstrap.min.css' />
+//         <script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossOrigin="anonymous"></script>
+//         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossOrigin="anonymous"></script>
+//       </Head>
+//       <Navbar />
+//       <div className="site-bg home-page-padding">
+//         {/* // Component holds pages where you navigation to, similar as children props */}
+//         <Component {...pageProps} />
+//       </div>
+//       <Footer />
+//       <style jsx>
+//         {`
+//             .home-page-padding {
+//               padding-top: 3rem;
+//             }
+
+//             .site-bg {
+//               background-color: #1a2b34;
+//             }  
+//         ` }
+//       </style>
+//     </>
+//   )
+// }
+
+// App.getInitialProps = async (appContext) => {
+
+//   console.log('calling initial props from _app.js');
+  
+//   const appProps = await App.getInitialProps(appContext)
 
 //   return { ...appProps }
 // }
 
-export default App
+// export default App
+
