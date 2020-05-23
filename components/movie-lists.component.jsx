96d790263221a811ab999d831156ad44550750ff
manitu85 <manitu.psy@gmail.com>
2020-05-24 00:02:53 +0200

    add movies as a dynamic page with router and id

--- components/movie-lists.component.jsx
+++ components/movie-lists.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Link from 'next/link'
 
 class MovieList extends Component {
 
@@ -14,12 +15,17 @@ class MovieList extends Component {
   renderMoviesList = movies => movies.map(movie => (
       <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
         <div className="card h-100">
-          <a href="#"><img className="card-img-top" src={movie.image} alt={movie.image} /></a>
+        <Link href={`/movies/${movie.id}`}>
+            <a><img className="card-img-top" src={movie.image} alt={movie.image} /></a>
+
+          </Link>
           <div className="card-body">
             <h6>{movie.releaseYear}</h6>
             <small className="text-muted">{movie.genre}</small>
             <h4 className="card-title">
-              <a href="#">{movie.name}</a>
+            <Link href={`/movies/${movie.id}`}>
+                <a >{movie.name}</a>
+              </Link>
             </h4>
           <p className="card-text">{this.shortenText(movie.description, 150)}</p>
           </div>
